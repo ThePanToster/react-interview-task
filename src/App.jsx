14712c88bff7492a00686e23f7e1c4440ebed970
ThePanToster <alcrafter@o2.pl>
2023-10-17 00:47:35 +0200

    Make basic form layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,93 @@
+import {
+    Box,
+    Paper,
+    Button,
+    Container,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    TextField,
+    Stack,
+    //FormHelperText,
+} from '@mui/material'
+
 function App() {
     return (
-        <>
-            <p>Hello world</p>
-        </>
+        <Container sx={{ marginTop: 20 }}>
+            <Paper component="form" elevation={8} sx={{ marginX: 20 }}>
+                <Container>
+                    <Stack py={5} px={22} spacing={2} alignItems={'center'}>
+                        <FormControl fullWidth>
+                            <InputLabel id="continent-dropdown-label">
+                                Kontynent
+                            </InputLabel>
+                            <Select
+                                label="Kontynent"
+                                id="continent-dropdown"
+                                //onChange={handleChange}
+                                //error
+                                //helperText="Incorrect entry."
+                            >
+                                <MenuItem value={'africa'}>Afryka</MenuItem>
+                                <MenuItem value={'north-america'}>
+                                    Ameryka Północna
+                                </MenuItem>
+                                <MenuItem value={'south-america'}>
+                                    Ameryka Południowa
+                                </MenuItem>
+                                <MenuItem value={'antarctica'}>
+                                    Antarktyda
+                                </MenuItem>
+                                <MenuItem value={'australia'}>
+                                    Australia
+                                </MenuItem>
+                                <MenuItem value={'asia'}>Azja</MenuItem>
+                                <MenuItem value={'europe'}>Europa</MenuItem>
+                            </Select>
+                            {/* <FormHelperText error>Niespełnione kryteria</FormHelperText> */}
+                        </FormControl>
+
+                        <Stack direction={'row'} gap={2}>
+                            <TextField
+                                //error
+                                id="name-input"
+                                label="Imię"
+                                //defaultValue="Imię"
+                                //helperText="To pole jest wymagane"
+                                required
+                            />
+                            <TextField
+                                //error
+                                id="surname-input"
+                                label="Nazwisko"
+                                //defaultValue="Imię"
+                                //helperText="To pole jest wymagane"
+                            />
+                        </Stack>
+
+                        <Box>
+                            <InputLabel id="date-input-label">
+                                Data urodzenia
+                            </InputLabel>
+                            <TextField
+                                //error
+                                id="date-input"
+                                labelId="date-input-label"
+                                //label="Data urodzenia"
+                                type="date"
+                                //defaultValue="Imię"
+                                //helperText="To pole jest wymagane"
+                            />
+                        </Box>
+
+                        <Button component="submit" variant="contained" disabled>
+                            Wyślij
+                        </Button>
+                    </Stack>
+                </Container>
+            </Paper>
+        </Container>
     )
 }
 
