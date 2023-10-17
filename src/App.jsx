8bfc238797d30fd511d78f3f0ee9917e5377e8fa
ThePanToster <alcrafter@o2.pl>
2023-10-17 15:48:20 +0200

    Change App to an arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
-import {
-    Container,
-    Stack,
-    //FormHelperText,
-} from '@mui/material'
+import { Container, Stack } from '@mui/material'
 
 import { Dropdown, NameInputs, DatePicker, Submit, Form } from './components'
 
-function App() {
-    return (
-        <Container sx={{ marginTop: 20 }}>
-            <Form>
-                <Stack py={5} px={22} spacing={2} alignItems={'center'}>
-                    <Dropdown />
-                    <NameInputs />
-                    <DatePicker />
-                    <Submit />
-                </Stack>
-            </Form>
-        </Container>
-    )
-}
+const App = () => (
+    <Container sx={{ marginTop: 20 }}>
+        <Form>
+            <Stack py={5} px={22} spacing={2} alignItems={'center'}>
+                <Dropdown />
+                <NameInputs />
+                <DatePicker />
+                <Submit />
+            </Stack>
+        </Form>
+    </Container>
+)
 
 export default App
