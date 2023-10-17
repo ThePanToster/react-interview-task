1bcea6fb8a145ccfa57528626ef4073e21f1ab5f
ThePanToster <alcrafter@o2.pl>
2023-10-17 22:03:16 +0200

    Add prop and fix state error in Dropdown

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,25 +1,39 @@
-import { FormControl, InputLabel, Select, MenuItem } from '@mui/material'
+import {
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    FormHelperText,
+} from '@mui/material'
 
-const Dropdown = () => (
-    <FormControl fullWidth>
-        <InputLabel id="continent-dropdown-label">Kontynent</InputLabel>
-        <Select
-            label="Kontynent"
-            id="continent-dropdown"
-            //onChange={handleChange}
-            //error
-            //helperText="Incorrect entry."
-        >
-            <MenuItem value={'africa'}>Afryka</MenuItem>
-            <MenuItem value={'north-america'}>Ameryka Północna</MenuItem>
-            <MenuItem value={'south-america'}>Ameryka Południowa</MenuItem>
-            <MenuItem value={'antarctica'}>Antarktyda</MenuItem>
-            <MenuItem value={'australia'}>Australia</MenuItem>
-            <MenuItem value={'asia'}>Azja</MenuItem>
-            <MenuItem value={'europe'}>Europa</MenuItem>
-        </Select>
-        {/* <FormHelperText error>Niespełnione kryteria</FormHelperText> */}
-    </FormControl>
-)
+import { useState } from 'react'
+
+// eslint-disable-next-line react/prop-types
+const Dropdown = ({ invalid }) => {
+    const [continent, setContinent] = useState('')
+    return (
+        <FormControl fullWidth>
+            <InputLabel id="continent-dropdown-label">Kontynent</InputLabel>
+            <Select
+                label="Kontynent"
+                name="continent"
+                error={invalid}
+                value={continent}
+                onChange={(e) => setContinent(e.target.value)}
+            >
+                <MenuItem value={'africa'}>Afryka</MenuItem>
+                <MenuItem value={'north-america'}>Ameryka Północna</MenuItem>
+                <MenuItem value={'south-america'}>Ameryka Południowa</MenuItem>
+                <MenuItem value={'antarctica'}>Antarktyda</MenuItem>
+                <MenuItem value={'australia'}>Australia</MenuItem>
+                <MenuItem value={'asia'}>Azja</MenuItem>
+                <MenuItem value={'europe'}>Europa</MenuItem>
+            </Select>
+            <FormHelperText error sx={{ display: invalid ? 'block' : 'none' }}>
+                Niespełnione kryteria
+            </FormHelperText>
+        </FormControl>
+    )
+}
 
 export default Dropdown
