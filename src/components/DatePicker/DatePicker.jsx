c387a18c232be2fda1a88c9671b4c7d2e632a5f0
ThePanToster <alcrafter@o2.pl>
2023-10-17 22:02:02 +0200

    Delete unnecessary comments from DatePicker

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -2,16 +2,8 @@ import { Box, TextField, InputLabel } from '@mui/material'
 
 const DatePicker = () => (
     <Box>
-        <InputLabel id="date-input-label">Data urodzenia</InputLabel>
-        <TextField
-            //error
-            id="date-input"
-            labelId="date-input-label"
-            //label="Data urodzenia"
-            type="date"
-            //defaultValue="Imię"
-            //helperText="To pole jest wymagane"
-        />
+        <InputLabel>Data urodzenia</InputLabel>
+        <TextField name="birthdate" type="date" />
     </Box>
 )
 
