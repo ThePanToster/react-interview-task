c44f9c295fb33c462d034d041893fc1e1d91b085
ThePanToster <alcrafter@o2.pl>
2023-10-17 22:00:53 +0200

    Add submitHandle prop to Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,11 @@
 import { Paper, Container } from '@mui/material'
 
 // eslint-disable-next-line react/prop-types
-const Form = ({ children }) => (
-    <Paper component="form" elevation={8} sx={{ marginX: 20 }}>
-        <Container>{children}</Container>
+const Form = ({ children, submitHandle }) => (
+    <Paper elevation={8} sx={{ marginX: 20 }}>
+        <Container component="form" onSubmit={submitHandle} noValidate>
+            {children}
+        </Container>
     </Paper>
 )
 
