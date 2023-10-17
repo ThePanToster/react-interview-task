57fcb655e49f6968767b54ec9f45f93d34986ac6
ThePanToster <alcrafter@o2.pl>
2023-10-17 15:39:17 +0200

    Add Submit component

---
+++ src/components/Submit/Submit.jsx
@@ -0,0 +1,9 @@
+import { Button } from '@mui/material'
+
+const Submit = () => (
+    <Button component="submit" variant="contained" disabled>
+        Wyślij
+    </Button>
+)
+
+export default Submit
